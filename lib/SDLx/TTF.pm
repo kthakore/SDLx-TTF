2b84166d7504c815e0caf8104dacb3181b43ec3a
Kartik Thakore <thakore.kartik@gmail.com>
2010-04-26 15:26:39 +0000

    First commit

---
+++ lib/SDLx/TTF.pm
@@ -0,0 +1,107 @@
+package SDLx::TTF;
+use strict;
+use warnings;
+use vars qw(@ISA @EXPORT @EXPORT_OK);
+require Exporter;
+require DynaLoader;
+use SDL::Constants ':SDL::TTF';
+our @ISA = qw(Exporter DynaLoader);
+
+use SDL::Internal::Loader;
+internal_load_dlls(__PACKAGE__);
+
+bootstrap SDLx::TTF;
+
+use base 'Exporter';
+our @EXPORT      = @{ $SDL::Constants::EXPORT_TAGS{'SDL::TTF'} };
+
+push( @EXPORT, 'SDL_TEXTWIDTH' );
+
+our %EXPORT_TAGS = (
+	all     => \@EXPORT,
+	hinting   => $SDL::Constants::EXPORT_TAGS{'SDL::TTF/hinting'},
+	style => $SDL::Constants::EXPORT_TAGS{'SDL::TTF/style'},
+);
+
+sub SDL_TEXTWIDTH {
+	return SDLx::TTF::TextWidth(join('',@_));
+}
+
+sub print_text{ #print is a horrible name for this
+	my ($surf, $x, $y, @text) = @_;
+	return SDLx::TTF::PutString( $surf, $x,$y,join('', @text));
+}
+
+
+
+1;
+
+#################### main pod documentation begin ###################
+
+
+=head1 NAME
+
+SDLx::TTF - Extenstions to SDL::TTF
+
+=head1 SYNOPSIS
+
+  use SDL;
+  use SDL::TTF;
+  use SDL::TTF::Font;
+
+  use SDLx::TTF;
+
+   #Make a surface
+   #Select a font
+
+   SDLx::TTF::print_text($surf, $x, $y, $text);
+
+   #that is it folks ..
+
+=head1 DESCRIPTION
+
+a simpler print function for old SDL::SFont dependency on Frozen-Bubble and Pangzero.
+
+Might actually be a usefull module some day ...
+
+=head1 USAGE
+
+see synopsis
+
+=head1 BUGS
+
+none
+
+=head1 SUPPORT
+
+#sdl irc.perl.org
+
+=head1 AUTHOR
+
+    Kartik Thakore
+    CPAN ID: KTHAKORE
+    ---
+    kthakore@cpan.org
+    http://sdl.perl.org
+
+=head1 COPYRIGHT
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+
+=head1 SEE ALSO
+
+perl(1), SDL(2).
+
+=cut
+
+#################### main pod documentation end ###################
+
+
+1;
+# The preceding line will help the module return a true value
+
